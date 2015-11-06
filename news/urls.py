d7d32b9a81891c670da1234bdc530dd64cba8480
ericcenter <Eric@EricdeMacBook-Air.local>
2015-11-06 16:12:20 +0800

    add JSON encode not work

--- news/urls.py
+++ news/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from parse.views import hello_world, parse
+from parse.views import parse
 
 
 urlpatterns = [
--- parse/views.py
+++ parse/views.py
@@ -5,25 +5,29 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 from django.http import HttpResponse
+from parse.models import News
+import json
 
 def hello_world(request):
 
-    return render(request,
-                  'parse.html',
-                  {'current_time': datetime.now()})
+    return HttpResponse("Hello World!")
 
 
 def parse(request):
     # get all the posts
-
 	res = requests.get ("https://news.google.com.tw/news/section?pz=1&ned=tw&topic=n&siidp=6ad8de07654b451fe8d2bb5fecb5c32affe3&ict=ln&sdm=EXPANDO&authuser=0")
 
 	soup = BeautifulSoup (res.text, "html.parser")
 	title =[]
 	n=0
 	for item in soup.select('.esc-body'):
-		title.append(item.select('span')[0])
+		#News.objects.create(title = item.select('span')[0].text.encode('utf-8'))
+		#title.append(item.select('span')[0].text)
+		title.append(json.dumps(item.select('span')[0].text,ensure_ascii=False))
     #news_list = News.objects.all()
 	return render(request,
                   'parse.html',
                   {'title': title})
+
+
+
